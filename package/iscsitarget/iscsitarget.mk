7aac44c12e92c7441f07d746057e1c7919a070b2
Ben Riedel <ben@go-unified.com>
2015-04-19 14:58:31 +0100

    Update Sources to Github raw

--- package/iscsitarget/iscsitarget.mk
+++ package/iscsitarget/iscsitarget.mk
@@ -3,7 +3,7 @@
 #
 ISCSITARGET_VERSION = r503
 ISCSITARGET_SOURCE = iscsitarget-1.4.20.2.tar.gz
-ISCSITARGET_SITE = http://www.go-unified.com/firebrick
+ISCSITARGET_SITE = https://raw.githubusercontent.com/benno16/extraFiles/master/package/sources
 
 ifeq ($(BR2_PACKAGE_ISCSITARGET_KMOD),y)
 ISCSITARGET_DEPENDENCIES = linux
--- package/stgt/stgt.mk
+++ package/stgt/stgt.mk
@@ -1,4 +1,4 @@
 STGT_VERSION=stgt-master
 STGT_SOURCE = stgt-master.tar.gz
-STGT_SITE = http://www.go-unified.com/firebrick
+STGT_SITE = https://raw.githubusercontent.com/benno16/extraFiles/master/package/sources
 $(eval $(autotools-package))
